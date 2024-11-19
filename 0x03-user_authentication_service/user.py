c269ffe3ebecbe1c7b65519c180bb4c7ea2c6c8f
Precious Ezeigbo <preciousezeigbo81@gmail.com>
2024-11-19 11:37:08 +0100

    Update

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
+"""User model"""
 from sqlalchemy import Column, Integer, String, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+
 class User(Base):
+    """Attributes of the User model"""
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True)
